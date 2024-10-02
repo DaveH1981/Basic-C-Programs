a0e353046ffc5c3e838aca5876be37a257430217
David Hunsdon <DaveH1981@users.noreply.github.com>
2024-10-02 01:08:28 -0700

    Update drink_age.cpp

--- drink_age.cpp
+++ drink_age.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
+
 int drinkAge = 21;
 int main ()
-{
-int yourAge = 15;
-cout << "You are " << yourAge <<
-"and not old enough to drink. " <<
-drinkAge << "." << endl;
   {
-  int momAge = 38;
-  cout << "Your mom is " << momAge
-  << ". She is old enough to drink "
-  << drinkAge << "." << endl;
+    int yourAge = 15;
+    cout << "You are " << yourAge <<
+    " and not old enough to drink. " << endl;
+    cout << "The legal drinking age is " << drinkAge << endl;
+  {
+    int momAge = 38;
+    cout << "Your mom is " << momAge
+    << ". She is old enough to drink." << endl;
   }
 }
