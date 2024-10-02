210ed50063f1ec899f443b05a55186da4f6fe395
David Hunsdon <DaveH1981@users.noreply.github.com>
2024-10-02 00:51:22 -0700

    Update dowhile.cpp
    Corrected the code after testing and fixing it in an online C++ compiler.

--- dowhile.cpp
+++ dowhile.cpp
@@ -1,22 +1,26 @@
 // An example of a do-while loop.
 #include <iostream>
+#include <cmath>
+#include <string>
 using namespace std;
-int main () 
-{
-float height;
-do
-  { cout << "How tall are you?";
-    cin >> height;
-if ((height < 3.5) || (height > 9.5))
-  { cout <<  "No way! You can't be that size!" << endl;
-    cout << "Tell me the truth...";
-  }
-  while ((height < 3.5)  || (height >9.5));}
-while (height < 4.5);
-  { cout << "You must be at least 4.5 feet tall to go on this ride." << endl;cout << "Sorry!" << endl;
-  }
-else 
-  { cout << "Enjoy the ride!.";
-  }
-return;
+
+int main () {
+    float height;
+        cout << "How tall are you in centimeters?";
+        cin >> height;
+    if ((height < 100) || (height > 250))
+    {
+       cout << "No Way! That can't be right!" << endl;
+       cout << "Please be honest...";
+    }   
+    else if (height < 135)
+    {
+        cout << "You must be at least 135cm tall to go on this ride." << endl;
+        cout << "Sorry!" << endl;
+    }
+    else 
+    {
+        cout << "Thanks! Enjoy the ride!";
+    }
+    return 0;
 }
