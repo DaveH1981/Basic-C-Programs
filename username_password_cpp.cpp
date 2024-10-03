8e91ff2303399fce020bcaed82a302e48a85a274
David Hunsdon <DaveH1981@users.noreply.github.com>
2024-10-02 20:52:26 -0700

    Add files via upload
    A rudimentary username/password program in C++.

---
+++ username_password_cpp.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    string username;
+    string password;
+    cout << "Please enter your username: ";
+    cin >> username; // Takes username input
+    cout << "Please enter your password: ";
+    cin >> password; // Takes password input
+
+    if (username == "SuperD00d" || password == "r!ghte0us") {
+        cout << "Logged in successfully!";
+    } else if (username == "user123" || password == "123456") {
+        cout << "Logged in successfully!";
+    } else if (username == "donkey_dave" || password == "Heatw@v3") {
+        cout << "Logged in successfully!";
+    } else {
+        cout << "Failed to login";
+    }
+    return 0;
+}
