d0af46e671ca2c01fe048b3e66fe151b174319f9
David Hunsdon <DaveH1981@users.noreply.github.com>
2024-10-03 01:27:19 -0700

    Update username_password_cpp.cpp

--- username_password_cpp.cpp
+++ username_password_cpp.cpp
@@ -11,11 +11,11 @@ int main ()
     cout << "Please enter your password: ";
     cin >> password; // Takes password input
 
-    if (username == "SuperD00d" || password == "r!ghte0us") {
+    if (username == "SuperD00d" && password == "r!ghte0us") {
         cout << "Logged in successfully!";
-    } else if (username == "user123" || password == "123456") {
+    } else if (username == "user123" && password == "123456") {
         cout << "Logged in successfully!";
-    } else if (username == "donkey_dave" || password == "Heatw@v3") {
+    } else if (username == "donkey_dave" && password == "Heatw@v3") {
         cout << "Logged in successfully!";
     } else {
         cout << "Failed to login";
